0253484cc36ab66fcdb9b434319658922be23177
Vitor Elbert <vitor.elbert@gmail.com>
2023-03-24 16:57:50 -0300

    API ALGA POO

---
+++ src/main/java/com/algaworks/algalog/domain/service/RegistroOcorrenciaService.java
@@ -0,0 +1,23 @@
+package com.algaworks.algalog.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.model.Ocorrencia;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class RegistroOcorrenciaService {
+
+	private BuscaEntregaService buscaEntregaService;
+	
+	@org.springframework.transaction.annotation.Transactional
+	public Ocorrencia registrar(Long entregaId, String descricao) {
+		Entrega entrega = buscaEntregaService.buscar(entregaId);
+		
+		return entrega.adicionarOcorrencia(descricao);
+	}
+	
+}
